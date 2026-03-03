6978819009b16f7ba560a79550c35da80d74bfba
Fernando_lara1 <fernando9096gab@gmail.com>
2026-03-03 23:41:26 +0100

    Agrega package.json

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Hola desde Node.js en Docker 🚀</h1>");
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
